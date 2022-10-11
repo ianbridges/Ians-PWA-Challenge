05e504884dfa992c7f74341408b00168dbc170af
ianbridges <megabridge12@gmail.com>
2022-10-11 12:05:48 -0500

    client<src<js<database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,41 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Post to database');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+
+  const result = await request;
+  if (result[0]) {
+    console.log("if side");
+    const data = result[0];
+    data.content = content;
+    store.put(data)
+  } else {
+    console.log("else side");
+    store.add({ jate: "jate", content:content })
+  }
+  console.log('data saved to database', result);
+};
+
+export const getDb = async () => {
+  console.log('GET from database');
+
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.obectStore('jate');
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('result.value', result);
+  return result[0];
+};
+
+console.log("database.js is running");
+
 
 initdb();
