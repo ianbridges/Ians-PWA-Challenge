624eca4d78345720798d31cf8f0d20614d65daad
ianbridges <megabridge12@gmail.com>
2022-10-11 13:23:35 -0500

    client<src<js<install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,24 @@
 const butInstall = document.getElementById('buttonInstall');
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    butInstall.style.visibility = 'visible';
+  
+    butInstall.addEventListener('click', () => {
+      event.prompt();
+      butInstall.setAttribute('disabled', true);
+      butInstall.textContent = 'Installed!';
+    });
+});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async (event) => {
+    event.preventDefault();
+        if (typeof CodeMirror === 'undefined') {
+            throw new Error('CodeMirror not loaded');
+          }
+          console.log(CodeMirror);
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log('👍', 'appinstalled', event);
+});
